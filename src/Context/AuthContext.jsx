73e8c9bcf263284ee73996ac5719a063f4e76a35
Akshansh Awasthi <108890778+awasthi2001@users.noreply.github.com>
2022-12-14 19:40:13 +0530

    Changing deployed link

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -9,11 +9,10 @@ export const AuthContextProvider = ({children}) => {
       let token = localStorage.getItem('bluemercury-token');
       if(token){
         (async ()=> {
-            let res = await fetch(`https://blure-mercury.herokuapp.com/Users?userToken=${token}`);
+            let res = await fetch(`https://nice-plum-dhole-gown.cyclic.app/Users?userToken=${token}`);
             let data = await res.json();
             setIsAuth({...isAuth,data:data,loggedin:true});
         })()
-        console.log(isAuth)
       }  
     },[isAuth.loggedin])
     console.log(isAuth);
