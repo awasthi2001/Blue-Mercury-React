f36931e4cca248f255fe187882f203db66aebe58
Akshansh Awasthi <108890778+awasthi2001@users.noreply.github.com>
2022-12-14 19:32:48 +0530

    Update deleteAllItemAfterOrder.jsx

--- src/pages/PaymentPages/deleteAllItemAfterOrder.jsx
+++ src/pages/PaymentPages/deleteAllItemAfterOrder.jsx
@@ -5,7 +5,7 @@ import { fetchAndUpdate } from "../../Redux/action"
 export const deleteAllItemAfterOrder = async (CartCount,Cart_Data,dispatch) => {
 try {
     for(let i=0; i<CartCount; i++) {
-       await fetch(`https://blure-mercury.herokuapp.com/Cart/${Cart_Data[i].id}`,{
+       await fetch(`https://nice-plum-dhole-gown.cyclic.app/Cart/${Cart_Data[i].id}`,{
         method : 'DELETE'
        })
     }
